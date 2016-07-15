4a69e78f73611a4c2d1092cb4ed8e764938c7ca3
Geza Kovacs <gkovacs@stanford.edu>
2016-07-15 16:58:51 -0700

    added addsignup

--- app.js
+++ app.js
@@ -1,10 +1,62 @@
 (function(){
-  var express, app, port, ref$;
+  var express, MongoClient, ref$, cfy, cfy_node, yfy_node, getDb, getSignupsCollection, app, port;
+  process.on('unhandledRejection', function(reason, p){
+    throw new Error(reason);
+  });
   express = require('express');
+  MongoClient = require('mongodb').MongoClient;
+  ref$ = require('cfy'), cfy = ref$.cfy, cfy_node = ref$.cfy_node, yfy_node = ref$.yfy_node;
+  getDb = cfy(function*(){
+    var mongourl, ref$, db;
+    mongourl = (ref$ = process.env.MONGODB_URI) != null ? ref$ : 'mongodb://localhost:27017/default';
+    db = (yield yfy_node(MongoClient.connect)(mongourl));
+    return db;
+  });
+  getSignupsCollection = cfy(function*(){
+    var db;
+    db = (yield getDb());
+    return db.collection('signups');
+  });
   app = express();
   port = (ref$ = process.env.PORT) != null ? ref$ : 5000;
   app.use(express['static'](__dirname + '/www'));
   app.set('port', port);
+  app.get('/addsignup', function(req, res){
+    var email;
+    email = req.query.email;
+    return getSignupsCollection(function(signups){
+      return signups.insertOne(req.query, {}, function(err, result){
+        res.send('done adding ' + email);
+      });
+    });
+  });
+  app.post('/addsignup', function(req, res){
+    var email;
+    email = req.body.email;
+    return getSignupsCollection(function(signups){
+      return signups.insertOne(req.body, {}, function(err, result){
+        res.send('done adding ' + email);
+      });
+    });
+  });
+  app.get('/getsignups', cfy(function*(req, res, next){
+    return getSignupsCollection(function(signups){
+      return signups.find({}).toArray(function(err, all_results){
+        var x;
+        res.send(JSON.stringify((function(){
+          var i$, ref$, len$, results$ = [];
+          for (i$ = 0, len$ = (ref$ = all_results).length; i$ < len$; ++i$) {
+            x = ref$[i$];
+            results$.push(x.email);
+          }
+          return results$;
+        }())));
+      });
+    });
+  }));
+  app.get('/addlog', cfy(function*(req, res, next){
+    res.send('addtolog called');
+  }));
   app.listen(app.get('port'));
   console.log("running on port " + port + " visit http://localhost:" + port);
 }).call(this);
--- app.ls
+++ app.ls
@@ -1,11 +1,51 @@
+process.on 'unhandledRejection', (reason, p) ->
+  throw new Error(reason)
+
 require! {
   express
 }
+
+{MongoClient} = require 'mongodb'
+{cfy, cfy_node, yfy_node} = require 'cfy'
+
+getDb = cfy ->*
+  mongourl = process.env.MONGODB_URI ? 'mongodb://localhost:27017/default'
+  #return mongojs(mongourl, ['signups'])
+  db = yield yfy_node(MongoClient.connect) mongourl
+  return db
+
+getSignupsCollection = cfy ->*
+  db = yield getDb()
+  return db.collection('signups')
 
 app = express()
 port = process.env.PORT ? 5000
 app.use express.static(__dirname + '/www')
 app.set 'port', port
+
+app.get '/addsignup', (req, res) ->
+  {email} = req.query
+  signups <- getSignupsCollection()
+  err,result <- signups.insertOne req.query, {}
+  res.send('done adding ' + email)
+  return
+
+app.post '/addsignup', (req, res) ->
+  {email} = req.body
+  signups <- getSignupsCollection()
+  err,result <- signups.insertOne req.body, {}
+  res.send('done adding ' + email)
+  return
+
+app.get '/getsignups', cfy (req, res, next) ->*
+  signups <- getSignupsCollection()
+  err,all_results <- signups.find({}).toArray()
+  res.send JSON.stringify([x.email for x in all_results])
+  return
+
+app.get '/addlog', cfy (req, res, next) ->*
+  res.send 'addtolog called'
+  return
 
 app.listen(app.get('port'))
 console.log "running on port #{port} visit http://localhost:#{port}"
