b30ec21a18364d30110c1ce9274e766db35662e1
Geza Kovacs <gkovacs@stanford.edu>
2016-07-19 18:48:50 -0700

    add_intervention_log implementation started, not yet finished

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  var koa, koaStatic, koaRouter, koaLogger, koaBodyparser, koaJsonp, monk, kapp, app, ref$, cfy, cfy_node, yfy_node, mongourl, db, signups, port;
+  var koa, koaStatic, koaRouter, koaLogger, koaBodyparser, koaJsonp, monk, kapp, app, ref$, cfy, cfy_node, yfy_node, mongourl, MongoClient, db, signups, get_native_db, list_collections, list_intervention_log_collections_for_user, list_intervention_log_collections_for_intervention, get_intervention_logs_for_user, port;
   process.on('unhandledRejection', function(reason, p){
     throw new Error(reason);
   });
@@ -17,30 +17,78 @@
   app = koaRouter();
   ref$ = require('cfy'), cfy = ref$.cfy, cfy_node = ref$.cfy_node, yfy_node = ref$.yfy_node;
   mongourl = (ref$ = process.env.MONGODB_URI) != null ? ref$ : 'mongodb://localhost:27017/default';
+  MongoClient = require('mongodb').MongoClient;
   db = monk(mongourl);
   signups = db.get('signups');
+  get_native_db = cfy(function*(){
+    return (yield function(it){
+      return MongoClient.connect(mongourl, it);
+    });
+  });
+  list_collections = cfy(function*(){
+    var ndb, collections_list, this$ = this;
+    ndb = (yield get_native_db());
+    collections_list = (yield function(it){
+      return ndb.listCollections().toArray(it);
+    });
+    return collections_list.map(function(it){
+      return it.name;
+    });
+  });
+  list_intervention_log_collections_for_user = cfy(function*(userid){
+    var all_collections;
+    all_collections = list_collections();
+    return all_collections.filter(function(it){
+      return it.startsWith("interventionlog_" + userid + "_");
+    });
+  });
+  list_intervention_log_collections_for_intervention = cfy(function*(intervention){
+    var all_collections;
+    all_collections = list_collections();
+    return all_collections.filter(function(it){
+      return it.startsWith("interventionlog_") && it.endsWith("_" + intervention_name);
+    });
+  });
+  get_intervention_logs_for_user = function(userid, intervention_name){
+    return db.get("interventionlogs_" + userid + "_" + intervention_name);
+  };
   app.get('/addsignup', function*(){
     var email, result;
+    this.type = 'json';
     email = this.request.query.email;
     if (email == null) {
-      this.body = 'need email parameter';
+      this.body = JSON.stringify({
+        response: 'error',
+        error: 'need parameter email'
+      });
       return;
     }
     result = (yield signups.insert(this.request.query));
-    return this.body = 'done adding ' + email;
+    return this.body = JSON.stringify({
+      response: 'done',
+      success: true
+    });
   });
   app.post('/addsignup', function*(){
     var email, result;
+    this.type = 'json';
     email = this.request.body.email;
     if (email == null) {
-      this.body = 'need email parameter';
+      this.body = JSON.stringify({
+        response: 'error',
+        error: 'need parameter email'
+      });
       return;
     }
     result = (yield signups.insert(this.request.body));
-    return this.body = 'done adding ' + email;
+    return this.body = JSON.stringify({
+      response: 'success',
+      success: true
+    });
   });
   app.get('/getsignups', function*(){
     var all_results, x;
+    this.type = 'json';
     all_results = (yield signups.find({}));
     return this.body = JSON.stringify((yield* (function*(){
       var i$, ref$, len$, results$ = [];
@@ -51,8 +99,46 @@
       return results$;
     }())));
   });
-  app.get('/addlog', function*(){
-    return this.body = 'addtolog called';
+  app.get('/get_logs_for_user', function*(){
+    var userid, user_collections;
+    this.type = 'json';
+    userid = this.request.body.userid;
+    if (userid == null) {
+      this.body = JSON.stringify({
+        response: 'error',
+        error: 'need parameter userid'
+      });
+      return;
+    }
+    user_collections = (yield list_intervention_log_collections_for_user(userid));
+    return this.body = JSON.stringify(user_collections);
+  });
+  app.get('/listcollections', function*(){
+    this.type = 'json';
+    return this.body = JSON.stringify((yield list_collections()));
+  });
+  app.post('/add_intervention_log', function*(){
+    var ref$, userid, intervention;
+    this.type = 'json';
+    ref$ = this.request.body, userid = ref$.userid, intervention = ref$.intervention;
+    if (userid == null) {
+      this.body = JSON.stringify({
+        response: 'error',
+        error: 'need parameter userid'
+      });
+      return;
+    }
+    if (intervention == null) {
+      this.body = JSON.stringify({
+        response: 'error',
+        error: 'need parameter intervention'
+      });
+      return;
+    }
+    return this.body = JSON.stringify({
+      response: 'error',
+      error: 'not yet implemented'
+    });
   });
   kapp.use(app.routes());
   kapp.use(app.allowedMethods());
--- app.ls
+++ app.ls
@@ -20,31 +20,76 @@ app = koa-router()
 {cfy, cfy_node, yfy_node} = require 'cfy'
 
 mongourl = process.env.MONGODB_URI ? 'mongodb://localhost:27017/default'
+{MongoClient} = require 'mongodb'
 db = monk(mongourl)
 signups = db.get 'signups'
 
+get_native_db = cfy ->*
+  yield -> MongoClient.connect mongourl, it
+
+list_collections = cfy ->*
+  ndb = yield get_native_db()
+  collections_list = yield -> ndb.listCollections().toArray(it)
+  return collections_list.map (.name)
+
+list_intervention_log_collections_for_user = cfy (userid) ->*
+  all_collections = list_collections()
+  return all_collections.filter -> it.startsWith("interventionlog_#{userid}_")
+
+list_intervention_log_collections_for_intervention = cfy (intervention) ->*
+  all_collections = list_collections()
+  return all_collections.filter -> it.startsWith("interventionlog_") and it.endsWith("_#{intervention_name}")
+
+get_intervention_logs_for_user = (userid, intervention_name) ->
+  return db.get "interventionlogs_#{userid}_#{intervention_name}"
+
 app.get '/addsignup', ->*
+  this.type = 'json'
   {email} = this.request.query
   if not email?
-    this.body = 'need email parameter'
+    this.body = JSON.stringify {response: 'error', error: 'need parameter email'}
     return
   result = yield signups.insert(this.request.query)
-  this.body = 'done adding ' + email
+  this.body = JSON.stringify {response: 'done', success: true}
 
 app.post '/addsignup', ->*
+  this.type = 'json'
   {email} = this.request.body
   if not email?
-    this.body = 'need email parameter'
+    this.body = JSON.stringify {response: 'error', error: 'need parameter email'}
     return
   result = yield signups.insert(this.request.body)
-  this.body = 'done adding ' + email
+  this.body = JSON.stringify {response: 'success', success: true}
 
 app.get '/getsignups', ->*
+  this.type = 'json'
   all_results = yield signups.find({})
   this.body = JSON.stringify([x.email for x in all_results])
 
-app.get '/addlog', ->*
-  this.body = 'addtolog called'
+app.get '/get_logs_for_user', ->*
+  this.type = 'json'
+  {userid} = this.request.body
+  if not userid?
+    this.body = JSON.stringify {response: 'error', error: 'need parameter userid'}
+    return
+  user_collections = yield list_intervention_log_collections_for_user(userid)
+  this.body = JSON.stringify user_collections
+
+app.get '/listcollections', ->*
+  this.type = 'json'
+  this.body = JSON.stringify yield list_collections()
+
+app.post '/add_intervention_log', ->*
+  this.type = 'json'
+  {userid, intervention} = this.request.body
+  if not userid?
+    this.body = JSON.stringify {response: 'error', error: 'need parameter userid'}
+    return
+  if not intervention?
+    this.body = JSON.stringify {response: 'error', error: 'need parameter intervention'}
+    return
+  this.body = JSON.stringify {response: 'error', error: 'not yet implemented'}
+  #this.body = JSON.stringify {response: 'success', success: true}
 
 kapp.use(app.routes())
 kapp.use(app.allowedMethods())
