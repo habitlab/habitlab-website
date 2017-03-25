d088a595dd21a79420286de031a313b511bdda6a
Geza Kovacs <gkovacs@stanford.edu>
2017-03-25 05:25:39 -0700

    added common route app_version

--- routes/common_routes.js
+++ routes/common_routes.js
@@ -1,8 +1,64 @@
 (function(){
-  var app;
+  var app, chromeWebStoreItemProperty, semver, appid_to_time_checked, appid_to_last_version;
   app = require('libs/server_common').app;
+  chromeWebStoreItemProperty = require('chrome-web-store-item-property');
+  semver = require('semver');
   app.get('/ping', function*(){
     this.body = 'hi';
+  });
+  appid_to_time_checked = {};
+  appid_to_last_version = {};
+  app.get('/app_version', function*(){
+    var appid, time_checked, ref$, current_time, app_info, version;
+    this.type = 'json';
+    appid = this.request.query.appid;
+    if (appid == null) {
+      this.body = {
+        response: 'error',
+        error: 'need appid'
+      };
+      return;
+    }
+    if (!(appid === 'obghclocpdgcekcognpkblghkedcpdgd' || appid === 'bleifeoekkfhicamkpadfoclfhfmmina')) {
+      this.body = {
+        response: 'error',
+        error: 'appid is not habitlab'
+      };
+      return;
+    }
+    time_checked = (ref$ = appid_to_time_checked[appid]) != null ? ref$ : 0;
+    current_time = Date.now();
+    if (time_checked + 1000 * 60 * 20 > current_time) {
+      this.body = {
+        response: 'success',
+        version: appid_to_last_version[appid]
+      };
+      return;
+    }
+    appid_to_time_checked[appid] = Date.now();
+    app_info = (yield chromeWebStoreItemProperty(appid));
+    version = app_info != null ? app_info.version : void 8;
+    if (!version) {
+      console.error('got empty version in app_version');
+      this.body = {
+        response: 'success',
+        version: appid_to_last_version[appid]
+      };
+      return;
+    }
+    if (!semver.valid(version)) {
+      console.error('got invalid version in app_version');
+      this.body = {
+        response: 'success',
+        version: appid_to_last_version[appid]
+      };
+      return;
+    }
+    appid_to_last_version[appid] = version;
+    this.body = {
+      response: 'success',
+      version: appid_to_last_version[appid]
+    };
   });
   app.get('/my_ip_address', function*(){
     this.body = this.request.ip_address_fixed;
--- routes/common_routes.ls
+++ routes/common_routes.ls
@@ -2,8 +2,45 @@
   app
 } = require 'libs/server_common'
 
+require! {
+  'chrome-web-store-item-property'
+  semver
+}
+
 app.get '/ping', ->*
   this.body = 'hi'
+  return
+
+appid_to_time_checked = {}
+appid_to_last_version = {}
+
+app.get '/app_version', ->*
+  this.type = 'json'
+  {appid} = this.request.query
+  if not appid?
+    this.body = {response: 'error', error: 'need appid'}
+    return
+  if not (appid == 'obghclocpdgcekcognpkblghkedcpdgd' or appid == 'bleifeoekkfhicamkpadfoclfhfmmina')
+    this.body = {response: 'error', error: 'appid is not habitlab'}
+    return
+  time_checked = appid_to_time_checked[appid] ? 0
+  current_time = Date.now()
+  if time_checked + 1000*60*20 > current_time # within the past 20 minutes
+    this.body = {response: 'success', version: appid_to_last_version[appid]}
+    return
+  appid_to_time_checked[appid] = Date.now()
+  app_info = yield chrome-web-store-item-property(appid)
+  version = app_info?version
+  if not version
+    console.error 'got empty version in app_version'
+    this.body = {response: 'success', version: appid_to_last_version[appid]}
+    return
+  if not semver.valid(version)
+    console.error 'got invalid version in app_version'
+    this.body = {response: 'success', version: appid_to_last_version[appid]}
+    return
+  appid_to_last_version[appid] = version
+  this.body = {response: 'success', version: appid_to_last_version[appid]}
   return
 
 app.get '/my_ip_address', ->*
