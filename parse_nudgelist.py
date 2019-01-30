d85ebf81a62ad89867e6c52eecf28ee05d871d3e
Geza Kovacs <geza0kovacs@gmail.com>
2019-01-30 00:15:42 -0800

    updated nudgelist

--- parse_nudgelist.py
+++ parse_nudgelist.py
@@ -1,6 +1,13 @@
 import csv
+import json
 
+output_list = []
 reader = csv.DictReader(open('nudgelist.csv'))
 for x in reader:
   idea = x['UI Implementation']
-  print(idea)
+  if idea == '[Not applicable]':
+  	continue
+  if idea == '':
+  	continue
+  output_list.append(idea)
+print(json.dumps({'generic/spend_less_time': output_list}, indent=2))
